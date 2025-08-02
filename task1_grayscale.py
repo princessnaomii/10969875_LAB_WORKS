9378a490bea119db4c7c56d65f5242d640d3b689
princessnaomii <103008328+princessnaomii@users.noreply.github.com>
2025-08-02 14:54:16 +0000

    Update task1_grayscale.py

--- task1_grayscale.py
+++ task1_grayscale.py
@@ -1,15 +1,15 @@
 import cv2
 
 # Load the original image
-image = cv2.imread('tree.jpg')
-cv2.imshow('Original Image', image)
 
-# Convert to grayscale
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Grayscale Image', gray_image)
+image = cv2.imread('tree.jpg')  
+cv2.imshow('Original - BGR', image)
+#coverting to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Grayscale', gray)
 
 # Save the grayscale image
-cv2.imwrite('photo_gray.jpg', gray_image)
+cv2.imwrite('grayscale.jpg', gray)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
